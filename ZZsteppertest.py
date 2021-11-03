ac035ff4bc1a2faadaa16c5e6a84d36fe69130c8
Allison Brown <91629048+alliebrown8800@users.noreply.github.com>
2021-11-03 15:12:36 +0000

    wait in test

--- ZZsteppertest.py
+++ ZZsteppertest.py
@@ -1,3 +1,4 @@
+import time
 from stepper import Stepper
 
 myMotor = Stepper() # the stepper motor
@@ -7,6 +8,8 @@ while True: # if slider was submitted
   new_angle = prev_angle + 60
   myMotor.goAngle(prev_angle,new_angle) # go to that angle
   prev_angle = new_angle # prev_angle is now this angle
+  time.sleep(3)
+  
 
 
 
