e9eef41a2940105b815b4bd1d8b90e3df4807bbd
Allison Brown <91629048+alliebrown8800@users.noreply.github.com>
2021-11-03 13:47:53 +0000

    testing stepper

---
+++ ZZsteppertest.py
@@ -0,0 +1,12 @@
+from stepper import Stepper
+
+myMotor = Stepper() # the stepper motor
+prev_angle = 0 # assume starting angle is zero
+
+while True: # if slider was submitted
+  new_angle = prev_angle + 60
+  myMotor.goAngle(prev_angle,new_angle) # go to that angle
+  prev_angle = new_angle # prev_angle is now this angle
+
+
+
